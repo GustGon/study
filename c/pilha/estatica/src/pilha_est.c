cb4d1c1ed64c276dce5b5b97d1faa41cf511bc82
Gustavo <gsilvagon@gmail.com>
2021-08-04 13:05:57 -0300

    Implement stack information

--- c/pilha/estatica/src/pilha_est.c
+++ c/pilha/estatica/src/pilha_est.c
@@ -15,6 +15,25 @@ void libera_Pilha(Pilha* pi){
     free(pi);
 }
 
+int tamanho_Pilha(Pilha* pi){
+    if(pi == NULL)
+        return -1;
+    else
+        return pi->qtd;
+}
+
+int Pilha_cheia(Pilha* pi){
+    if(pi == NULL)
+        return -1;
+    return (pi->qtd == MAX);
+}
+
+int Pilha_vazia(Pilha* pi){
+    if( pi == NULL )
+        return -1;
+    return (pi->qtd == 0);
+}
+
 int main(){
     Pilha *pi;
 
--- c/pilha/estatica/src/pilha_est.h
+++ c/pilha/estatica/src/pilha_est.h
@@ -9,3 +9,6 @@ typedef struct pilha Pilha;
 
 Pilha* cria_Pilha();
 void libera_Pilha(Pilha* pi);
+int tamanho_Pilha(Pilha* pi);
+int Pilha_cheia(Pilha* pi);
+int Pilha_vazia(Pilha* pi);
